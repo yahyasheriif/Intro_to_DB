5596c7e6300bbef98bbb24a769d42939028a1eac
yahyasheriif <149334812+yahyasheriif@users.noreply.github.com>
2024-10-20 20:57:38 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,4 @@
+-- Insert multiple rows into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
